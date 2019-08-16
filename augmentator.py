854a08f39bb2d8f1954b2d797f586c717e4c5f84
ecovirtual <ramiro.ecovirtual@gmail.com>
2019-08-16 16:13:48 +0200

    Add basic transformation and CLI parameters

--- augmentator.py
+++ augmentator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 @author: Ramiro Ramirez
 
@@ -9,9 +11,17 @@ import Augmentor
 import argparse
 
 # Add CLI argument parser
-parser = argparse.ArgumentParser(description='Process each image in a directory and outputs a new dataset with the transformations.')
-parser.add_argument("--input_dir", default="./dataset/input", help="path to directory containing the images")
-# parser.add_argument("--output_dir", default="./dataset/output", help="path to directory to save new images")
+parser = argparse.ArgumentParser(description="Process each image in a directory and outputs a new dataset with the transformations.")
+parser.add_argument("--input_dir", default="../datasets/databetes/gummybears_macro", help="path to directory containing the images")
+parser.add_argument("--output_dir", default="../datasets/databetes/gummybears_macro/output", help="path to directory to save new images")
+parser.add_argument("--output_format", default="JPEG", choices=["JPEG","BMP","PNG","GIF"], help="output image format (JPEG|BMP|PNG|GIF)")
+parser.add_argument("--output_width", default=512, help="output image width in pixels")
+parser.add_argument("--output_height", default=512, help="output image height in pixels")
+parser.add_argument("--resample_filter", default="BICUBIC", choices=["BICUBIC","BILINEAR","ANTIALIAS","NEAREST"], help="filter to use when resampling (BICUBIC|BILINEAR|ANTIALIAS|NEAREST)")
+parser.add_argument("--sample_count", default=100, help="amount of new images to generate")
+parser.add_argument("--zoom", action="store_true", help="zoom in randomly")
+parser.add_argument("--distortion", action="store_true", help="distort randomly")
+
 
 # TODO Add argument to use image pairs as input (to process cGAN datasets)
 
@@ -21,21 +31,30 @@ a = parser.parse_args()
 # in_path = os.path.dirname(a.input_dir)
 # out_path = os.path.dirname(a.output_dir)
 
-# Create output path if doesn't exist
+# Create output path if doesn"t exist
 # if os.path.exists(out_path) == False:
 #     os.makedirs(out_path)
 
 # Create an augmentation pipeline
-p = Augmentor.Pipeline(a.input_dir)
+p = Augmentor.Pipeline(source_directory=a.input_dir, output_directory=a.output_dir, save_format=a.output_format)
 
 # Define the transformations
-p.rotate90(probability=0.5)
-p.rotate270(probability=0.5)
-p.flip_left_right(probability=1)
-p.flip_top_bottom(probability=1)
-p.crop_random(probability=.8, percentage_area=0.8)
-p.resize(probability=1, width=512, height=512)
+
+# p.rotate90(probability=0.5)
+# p.rotate270(probability=0.5)
+# p.flip_left_right(probability=1)
+# p.flip_top_bottom(probability=1)
+
+if a.zoom:
+    p.zoom_random(probability=0.8, percentage_area=0.8, randomise_percentage_area=False)
+    # p.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
+
+if a.distortion:
+    p.random_distortion(probability=0.8, grid_width=10, grid_height=10, magnitude=6)
+
+# p.resize(probability=1, width=a.output_width, height=a.output_height, resample_filter=a.resample_filter)
+p.crop_by_size(probability=1, width=a.output_width, height=a.output_height)
 
 # Generate new augmented images
 p.status()
-p.sample(500)
+p.sample(10)
