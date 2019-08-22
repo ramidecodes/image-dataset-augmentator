bd448b42871784c0e902cb86000fdea5741e811a
ecovirtual <ramiro.ecovirtual@gmail.com>
2019-08-22 11:11:13 +0200

    Adda sample count as parameter

--- augmentator.py
+++ augmentator.py
@@ -14,13 +14,13 @@ import argparse
 parser = argparse.ArgumentParser(description="Process each image in a directory and outputs a new dataset with the transformations.")
 parser.add_argument("--input_dir", default="../datasets/databetes/gummybears_macro", help="path to directory containing the images")
 parser.add_argument("--output_dir", default="../datasets/databetes/gummybears_macro/output", help="path to directory to save new images")
-parser.add_argument("--output_format", default="JPEG", choices=["JPEG","BMP","PNG","GIF"], help="output image format (JPEG|BMP|PNG|GIF)")
+parser.add_argument("--output_format", default="JPEG", choices=["JPEG","BMP","PNG","GIF"], help="output image format")
 parser.add_argument("--output_width", default=512, help="output image width in pixels")
 parser.add_argument("--output_height", default=512, help="output image height in pixels")
-parser.add_argument("--resample_filter", default="BICUBIC", choices=["BICUBIC","BILINEAR","ANTIALIAS","NEAREST"], help="filter to use when resampling (BICUBIC|BILINEAR|ANTIALIAS|NEAREST)")
-parser.add_argument("--sample_count", default=100, help="amount of new images to generate")
+parser.add_argument("--resample_filter", default="BICUBIC", choices=["BICUBIC","BILINEAR","ANTIALIAS","NEAREST"], help="filter to use when resampling")
 parser.add_argument("--zoom", action="store_true", help="zoom in randomly")
 parser.add_argument("--distortion", action="store_true", help="distort randomly")
+parser.add_argument("--sample_count", default=100, help="amount of new images to generate")
 
 
 # TODO Add argument to use image pairs as input (to process cGAN datasets)
@@ -58,4 +58,4 @@ if a.distortion:
 
 # Generate new augmented images
 p.status()
-p.sample(10)
+p.sample(a.sample_count)
